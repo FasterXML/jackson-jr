c790ec390e92cfb64ae94274dd05b31f8072f6e8
Tatu Saloranta <tatu.saloranta@iki.fi>
2014-03-18 19:13:47 -0700

    ...

--- jr-objects/src/test/java/com/fasterxml/jackson/jr/ob/ReadBeansTest.java
+++ jr-objects/src/test/java/com/fasterxml/jackson/jr/ob/ReadBeansTest.java
@@ -1,7 +1,6 @@
 package com.fasterxml.jackson.jr.ob;
 
 import com.fasterxml.jackson.jr.ob.JSON;
-//import com.fasterxml.jackson.jr.ob.JSON.Feature;
 
 public class ReadBeansTest extends TestBase
 {
@@ -26,28 +25,13 @@ public class ReadBeansTest extends TestBase
         public void setLast(String n) { last = n; }
     }
 
-    static class FromString {
-        protected String value;
-        public FromString(String v) { value = v; }
-    }
-
-    static class FromLong1 {
-        protected long value;
-        public FromLong1(long v) { value = v; }
-    }
-
-    static class FromLong2 {
-        protected long value;
-        public FromLong2(Long v) { value = v.longValue(); }
-    }
-    
     /*
     /**********************************************************************
     /* Test methdods
     /**********************************************************************
      */
     
-    public void testSimpleList() throws Exception
+    public void testSimpleBean() throws Exception
     {
         final String INPUT = aposToQuotes("{'name':{'first':'Bob','last':'Burger'},'x':13}");
         TestBean bean = JSON.std.beanFrom(INPUT, TestBean.class);
@@ -57,23 +41,5 @@ public class ReadBeansTest extends TestBase
         assertNotNull(bean.name);
         assertEquals("Bob", bean.name.first);
         assertEquals("Burger", bean.name.last);
-    }
-
-    public void testStringCtor() throws Exception
-    {
-        FromString output = JSON.std.beanFrom(quote("abc"), FromString.class);
-        assertNotNull(output);
-        assertEquals("abc", output.value);
-    }
-
-    public void testLongCtor() throws Exception
-    {
-        FromLong1 output = JSON.std.beanFrom("123", FromLong1.class);
-        assertNotNull(output);
-        assertEquals(123L, output.value);
-
-        FromLong2 output2 = JSON.std.beanFrom("456", FromLong2.class);
-        assertNotNull(output2);
-        assertEquals(456L, output2.value);
     }
 }
---
+++ jr-objects/src/test/java/com/fasterxml/jackson/jr/ob/ReadWithCtorsTest.java
@@ -0,0 +1,45 @@
+package com.fasterxml.jackson.jr.ob;
+
+import com.fasterxml.jackson.jr.ob.JSON;
+
+public class ReadWithCtorsTest extends TestBase
+{
+    static class FromString {
+        protected String value;
+        public FromString(String v) { value = v; }
+    }
+
+    static class FromLong1 {
+        protected long value;
+        public FromLong1(long v) { value = v; }
+    }
+
+    static class FromLong2 {
+        protected long value;
+        public FromLong2(Long v) { value = v.longValue(); }
+    }
+    
+    /*
+    /**********************************************************************
+    /* Test methdods
+    /**********************************************************************
+     */
+
+    public void testStringCtor() throws Exception
+    {
+        FromString output = JSON.std.beanFrom(quote("abc"), FromString.class);
+        assertNotNull(output);
+        assertEquals("abc", output.value);
+    }
+
+    public void testLongCtor() throws Exception
+    {
+        FromLong1 output = JSON.std.beanFrom("123", FromLong1.class);
+        assertNotNull(output);
+        assertEquals(123L, output.value);
+
+        FromLong2 output2 = JSON.std.beanFrom("456", FromLong2.class);
+        assertNotNull(output2);
+        assertEquals(456L, output2.value);
+    }
+}
