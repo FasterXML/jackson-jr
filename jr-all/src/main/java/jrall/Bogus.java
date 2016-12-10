bfc98170f7224fc4b83c58b17f52f895630ab3d2
Tatu Saloranta <tatu.saloranta@iki.fi>
2016-12-09 17:02:13 -0800

    remove a bogus file (leftover from something)

--- jr-all/src/main/java/jrall/Bogus.java
+++
@@ -1,7 +0,0 @@
-/* This class is only included to trigger production of source and javadoc
- * jars; this doesn't seem to happen when merely shading external things.
- * Silly, yes, would be nice to get rid of this.
- */
-package jrall;
-
-public interface Bogus { }
