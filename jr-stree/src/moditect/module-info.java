71884c6067b9a89a97d1e328977f493aab3d0557
Tatu Saloranta <tatu.saloranta@iki.fi>
2019-03-11 19:48:30 -0700

    Add Moditect-generated module info baseline settings (possibly wrong wrt Retrofit deps but)

---
+++ jr-stree/src/moditect/module-info.java
@@ -0,0 +1,8 @@
+// Generated 11-Mar-2019 using Moditect maven plugin
+module com.fasterxml.jackson.jr.stree {
+    requires transitive com.fasterxml.jackson.core;
+
+    exports com.fasterxml.jackson.jr.stree;
+    exports com.fasterxml.jackson.jr.stree.util;
+
+}
