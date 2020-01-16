725bf068d55628c75f5a13bdabd075e742df945d
Tatu Saloranta <tatu.saloranta@iki.fi>
2020-01-15 16:55:01 -0800

    add module info for new subproject

---
+++ jr-annotation-support/src/moditect/module-info.java
@@ -0,0 +1,9 @@
+module com.fasterxml.jackson.jr.annotationsupport {
+    requires com.fasterxml.jackson.core;
+
+    requires com.fasterxml.jackson.jr.ob;
+    requires com.fasterxml.jackson.jr.ob.api;
+    requires com.fasterxml.jackson.jr.ob.impl;
+
+    exports com.fasterxml.jackson.jr.annotationsupport;
+}
