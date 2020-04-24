f6e98138f7858dba5c2c6dcfa351c772b6c53d9d
Tatu Saloranta <tatu.saloranta@iki.fi>
2020-04-23 20:05:27 -0700

    Add failign test for #51, just in case

--- jr-stree/src/test/java/com/fasterxml/jackson/jr/stree/ReadViaJSONTest.java
+++ jr-stree/src/test/java/com/fasterxml/jackson/jr/stree/ReadViaJSONTest.java
@@ -12,53 +12,53 @@ import com.fasterxml.jackson.jr.ob.JSON;
  */
 public class ReadViaJSONTest extends TestBase
 {
-     private final TreeCodec TREE_CODEC = new JacksonJrsTreeCodec();
-     private final JSON treeJSON = jsonWithTreeCodec();
+    private final TreeCodec TREE_CODEC = new JacksonJrsTreeCodec();
+    private final JSON treeJSON = jsonWithTreeCodec();
 
-     public void testSimpleList() throws Exception
-     {
-          final String INPUT = "[true,\"abc\"]";
-         // and then through jr-objects:
-          TreeNode node = treeJSON.treeFrom(INPUT);
+    public void testSimpleList() throws Exception
+    {
+        final String INPUT = "[true,\"abc\"]";
+        // and then through jr-objects:
+        TreeNode node = treeJSON.treeFrom(INPUT);
 
-         assertTrue(node instanceof JrsArray);
-         assertEquals(2, node.size());
-         // actually, verify with write...
-         final StringWriter writer = new StringWriter();
-         final JsonGenerator g = _factory.createGenerator(writer);
-         TREE_CODEC.writeTree(g, node);
-         g.close();
-         assertEquals(INPUT, writer.toString());
-     }
+        assertTrue(node instanceof JrsArray);
+        assertEquals(2, node.size());
+        // actually, verify with write...
+        final StringWriter writer = new StringWriter();
+        final JsonGenerator g = _factory.createGenerator(writer);
+        TREE_CODEC.writeTree(g, node);
+        g.close();
+        assertEquals(INPUT, writer.toString());
+    }
 
-     public void testSimpleMap() throws Exception
-     {
-         final String INPUT = "{\"a\":1,\"b\":true,\"c\":3}";
-         TreeNode node = treeJSON.treeFrom(INPUT);
-         assertTrue(node instanceof JrsObject);
-         assertEquals(3, node.size());
-         // actually, verify with write...
-         final StringWriter writer = new StringWriter();
-         final JsonGenerator g = _factory.createGenerator(writer);
-         TREE_CODEC.writeTree(g, node);
-         g.close();
-         assertEquals(INPUT, writer.toString());
-     }
+    public void testSimpleMap() throws Exception
+    {
+        final String INPUT = "{\"a\":1,\"b\":true,\"c\":3}";
+        TreeNode node = treeJSON.treeFrom(INPUT);
+        assertTrue(node instanceof JrsObject);
+        assertEquals(3, node.size());
+        // actually, verify with write...
+        final StringWriter writer = new StringWriter();
+        final JsonGenerator g = _factory.createGenerator(writer);
+        TREE_CODEC.writeTree(g, node);
+        g.close();
+        assertEquals(INPUT, writer.toString());
+    }
 
-     public void testSimpleMixed() throws Exception
-     {
-         final String INPUT = "{\"a\":[1,2,{\"b\":true},3],\"c\":3}";
-         TreeNode node = treeJSON.treeFrom(INPUT);
-         assertTrue(node instanceof JrsObject);
-         assertEquals(2, node.size());
-         TreeNode list = node.get("a");
-         assertTrue(list instanceof JrsArray);
+    public void testSimpleMixed() throws Exception
+    {
+        final String INPUT = "{\"a\":[1,2,{\"b\":true},3],\"c\":3}";
+        TreeNode node = treeJSON.treeFrom(INPUT);
+        assertTrue(node instanceof JrsObject);
+        assertEquals(2, node.size());
+        TreeNode list = node.get("a");
+        assertTrue(list instanceof JrsArray);
 
-         // actually, verify with write...
-         final StringWriter writer = new StringWriter();
-         final JsonGenerator g = _factory.createGenerator(writer);
-         TREE_CODEC.writeTree(g, node);
-         g.close();
-         assertEquals(INPUT, writer.toString());
-     }
+        // actually, verify with write...
+        final StringWriter writer = new StringWriter();
+        final JsonGenerator g = _factory.createGenerator(writer);
+        TREE_CODEC.writeTree(g, node);
+        g.close();
+        assertEquals(INPUT, writer.toString());
+    }
 }
