fce2522df6f21e773fb9f0a10cd4ae17e9fed861
Shalnark <65479699+Shounaks@users.noreply.github.com>
2024-02-12 18:16:51 +0530

    Fixing Module related files.

---
+++ jr-extension-datetime/src/moditect/module-info.java
@@ -0,0 +1,6 @@
+module com.fasterxml.jackson.jr.extension.datetime {
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.jr.ob;
+
+    exports com.fasterxml.jackson.jr.extension.datetime;
+}
