66cf5b3faa2977a681f4aaf752131ebe10e70c80
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-02-19 19:49:18 -0800

    Change post #116 to potentially change serialization of `java.nio.file.Path`

--- jr-objects/src/main/java/com/fasterxml/jackson/jr/ob/impl/JSONWriter.java
+++ jr-objects/src/main/java/com/fasterxml/jackson/jr/ob/impl/JSONWriter.java
@@ -789,7 +789,8 @@ public class JSONWriter
      * @since 2.17
      */
     protected String pathToString(Path value) {
-        return value.toUri().toString();
+//        return value.toUri().toString();
+        return value.toString();
     }
 
     /*
