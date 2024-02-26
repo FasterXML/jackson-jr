4ebde3eccf8bd46086146abbf266a29651445e69
Shalnark <65479699+Shounaks@users.noreply.github.com>
2024-02-26 07:51:59 +0530

    minor code cleanup + adding tests

--- jr-objects/src/test/java/com/fasterxml/jackson/jr/ob/ReadIntArray7Test.java
+++ jr-objects/src/test/java/com/fasterxml/jackson/jr/ob/ReadIntArray7Test.java
@@ -12,4 +12,18 @@ public class ReadIntArray7Test extends TestBase {
         int[] result = JSON.std.beanFrom(int[].class, json);
         assertArrayEquals(input, result);
     }
+
+    public void testReadIntArray2() throws Exception {
+        final int[][] input = new int[][]{{1, 2, 3, 25, 999},{456,678,789},{1},{},{1000,2000,3000}};
+        String json = JSON.std.asString(input);
+        int[][] result = JSON.std.beanFrom(int[][].class, json);
+        assertArrayEquals(input, result);
+    }
+
+    public void testReadIntArray3() throws Exception {
+        final int[][][] input = new int[][][]{{{1, 2, 3, 25, 999},{6,7,3}},{{456}, {678, 789}},{},{{},{23}},{{}}};
+        String json = JSON.std.asString(input);
+        int[][][] result = JSON.std.beanFrom(int[][][].class, json);
+        assertArrayEquals(input, result);
+    }
 }
