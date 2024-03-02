7b81bda1beb661684b1680199a504991b0baa1e5
Shalnark <65479699+Shounaks@users.noreply.github.com>
2024-03-02 15:57:09 +0530

    Adding maven-compiler plugin to run test on specific JDK, Reducing JDK version to 17 + other cleanup changes.

--- jr-test-module/src/moditect/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.fasterxml.jackson.jr.annotationsupport {
-    requires com.fasterxml.jackson.core;
-    requires com.fasterxml.jackson.jr.ob;
-    requires com.fasterxml.jackson.jr.ob.api;
-    requires com.fasterxml.jackson.jr.ob.comp;
-    requires com.fasterxml.jackson.jr.ob.impl;
-    requires com.fasterxml.jackson.jr.type;
-    requires com.fasterxml.jackson.annotation;
-}
