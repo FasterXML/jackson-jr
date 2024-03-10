7b9566988fa865d893cfa21119628703e1253472
Shalnark <65479699+Shounaks@users.noreply.github.com>
2024-03-10 06:55:40 +0530

    fix: remove now-redundant method

--- jr-objects/src/main/java/com/fasterxml/jackson/jr/ob/impl/POJODefinition.java
+++ jr-objects/src/main/java/com/fasterxml/jackson/jr/ob/impl/POJODefinition.java
@@ -176,10 +176,6 @@ public class POJODefinition
             _isGetter = m;
             return this;
         }
-
-        public Field get_field() {
-            return _field;
-        }
     }
 }
 
