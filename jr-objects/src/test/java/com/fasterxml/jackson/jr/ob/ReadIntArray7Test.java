0d981a46f05cc1f9740522302f73338080ee3843
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-02-02 18:16:57 -0800

    Update Retrofit dep (2.9 -> 2.11), module deps

--- jr-objects/src/test/java/com/fasterxml/jackson/jr/ob/ReadIntArray7Test.java
+++ jr-objects/src/test/java/com/fasterxml/jackson/jr/ob/ReadIntArray7Test.java
@@ -3,7 +3,6 @@ package com.fasterxml.jackson.jr.ob;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReadIntArray7Test extends TestBase
 {
